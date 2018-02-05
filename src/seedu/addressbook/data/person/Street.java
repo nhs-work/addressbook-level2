fefda0cdd8981d599ae3cb693ccda54247b45891
DESKTOP-219H7BI\NgHungSiang <nhs.work.95@gmail.com>
2018-02-05 16:30:36 +0800

    Added in block street

---
+++ src/seedu/addressbook/data/person/Street.java
@@ -0,0 +1,35 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's block number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Street {
+
+    public static final String EXAMPLE = "123c";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Block addresses can be in any format without any whitespace characters";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+
+    public final String value;
+
+    /**
+     * Validates given address.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public Street(String street, boolean isPrivate) throws IllegalValueException {
+        String trimmedStreet = street.trim();
+        if (!isValidAddress(trimmedStreet)) {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        this.value = trimmedStreet;
+    }
+
+    /**
+     * Returns true if a given string is a valid person address.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(ADDRESS_VALIDATION_REGEX);
+    }
