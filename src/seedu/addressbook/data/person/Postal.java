62a53387bdefd4069dd4756273d50bd396edfee3
DESKTOP-219H7BI\NgHungSiang <nhs.work.95@gmail.com>
2018-02-05 16:44:57 +0800

    Added in postal class

---
+++ src/seedu/addressbook/data/person/Postal.java
@@ -0,0 +1,35 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's block number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Postal {
+
+    public static final String EXAMPLE = "123c";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Potal code can only be numeric";
+    public static final String ADDRESS_VALIDATION_REGEX = "[123456789]+";
+
+    public final String value;
+
+    /**
+     * Validates given address.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public Postal(String postal, boolean isPrivate) throws IllegalValueException {
+        String trimmedPostal = postal.trim();
+        if (!isValidAddress(trimmedPostal)) {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        this.value = trimmedPostal;
+    }
+
+    /**
+     * Returns true if a given string is a valid person address.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(ADDRESS_VALIDATION_REGEX);
+    }
