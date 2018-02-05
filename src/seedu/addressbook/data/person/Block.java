9444ab397d15d94239ef8c99d06199ab1a62f822
DESKTOP-219H7BI\NgHungSiang <nhs.work.95@gmail.com>
2018-02-05 16:33:14 +0800

    Added in block class

---
+++ src/seedu/addressbook/data/person/Block.java
@@ -0,0 +1,35 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's block number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Block {
+
+    public static final String EXAMPLE = "123c";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Block addresses can be in any format without any whitespace characters";
+    public static final String ADDRESS_VALIDATION_REGEX = "\\S+";
+
+    public final String value;
+
+    /**
+     * Validates given address.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public Block(String block, boolean isPrivate) throws IllegalValueException {
+        String trimmedBLock = block.trim();
+        if (!isValidAddress(trimmedBLock)) {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        this.value = trimmedBLock;
+    }
+
+    /**
+     * Returns true if a given string is a valid person address.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(ADDRESS_VALIDATION_REGEX);
+    }
