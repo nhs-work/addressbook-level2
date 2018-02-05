e87ad76bc2334881a6dc0a54f053739af5140145
DESKTOP-219H7BI\NgHungSiang <nhs.work.95@gmail.com>
2018-02-05 16:45:08 +0800

    Added in unit class

---
+++ src/seedu/addressbook/data/person/Unit.java
@@ -0,0 +1,35 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+/**
+ * Represents a Person's block number in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Unit {
+
+    public static final String EXAMPLE = "123c";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Unit number must be numeric or "-" only";
+    public static final String ADDRESS_VALIDATION_REGEX = "[123456789-]+";
+
+    public final String value;
+
+    /**
+     * Validates given address.
+     *
+     * @throws IllegalValueException if given address string is invalid.
+     */
+    public Unit(String unit, boolean isPrivate) throws IllegalValueException {
+        String trimmedUnit = unit.trim();
+        if (!isValidAddress(trimmedUnit)) {
+            throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
+        }
+        this.value = trimmedUnit;
+    }
+
+    /**
+     * Returns true if a given string is a valid person address.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(ADDRESS_VALIDATION_REGEX);
+    }
