5ed2902cbe05fe66f3e5448d509d6db2df074443
nhs-work <nhs.work.95@gmail.com>
2018-02-10 18:11:25 +0800

    Added new sort command (shell of list command) and updated parser to recognise new command

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Sorts all persons in the address book before displaying the new sorted list to the user.
+ */
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts all persons in the address book before displaying all persons in the address book as a list with index numbers.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
