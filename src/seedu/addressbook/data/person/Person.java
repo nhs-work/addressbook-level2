be9374bb4fe800a3f096e9385b1af80ff058ab5a
nhs-work <nhs.work.95@gmail.com>
2018-02-12 22:59:03 +0800

    Include method to get sequence number, change privacy setting of sequence number

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -64,6 +64,11 @@ public class Person implements ReadOnlyPerson {
         return new UniqueTagList(tags);
     }
 
+    @Override
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
     /**
      * Replaces this person's tags with the tags in the argument tag list.
      */
