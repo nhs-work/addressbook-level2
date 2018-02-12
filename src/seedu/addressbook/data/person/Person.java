13853c70181a31d9345efbfc798e202f5d4b93c2
nhs-work <nhs.work.95@gmail.com>
2018-02-12 23:19:06 +0800

    Change method of incrementing nextSequenceNumber

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -29,7 +29,7 @@ public class Person implements ReadOnlyPerson {
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
         this.sequenceNumber = nextSequenceNumber;
-        nextSequenceNumber += 1;
+        nextSequenceNumber = this.sequenceNumber + 1;
     }
 
     /**
